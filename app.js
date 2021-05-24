fbda0cc5180975a6671e1649b80120cff672adf0
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-05-25 00:00:42 +0300

    template engine eklendi

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-const fn = function () {
-  console.log('selam');
-};
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+const app = express();
+
+//Template Engine
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
+app.get('/', (req, res) => {
+
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+
+  res.render('about');
+
+  app.get('/add', (req, res) => {
+
+    res.render('add');
+  });
+  
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
