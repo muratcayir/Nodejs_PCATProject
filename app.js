2bf7a98ef1b0f8ad5c18f475ca080c940f2fee91
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-05-26 16:41:18 +0300

    MongoDB ve CRUD eklendi

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
+const Photo = require('./models/Photo')
+
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))//URL okunmasını sağlıyor
+app.use(express.json())// Datayı JSON formatına çeviriyor
 
 //ROUTES
-app.get('/', (req, res) => {
-
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index',
+ { photos});
 });
 
 app.get('/about', (req, res) => {
-
   res.render('about');
-
-  app.get('/add', (req, res) => {
-
-    res.render('add');
-  });
-  
 });
 
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async  (req, res) => {
+ await Photo.create(req.body)
+ res.redirect("/")
+});
 
 const port = 3000;
 app.listen(port, () => {
